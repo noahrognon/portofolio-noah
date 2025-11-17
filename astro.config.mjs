c7e5e740afcf600a61204af680cf27b314e2faf1
noahrognon <noah.rognon@edu.univ-fcomte.fr>
2025-11-17 11:22:18 +0100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import node from "@astrojs/node";
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  output: "server", // obligatoire pour SSR
+  adapter: node({
+    mode: "standalone" // génère dist/server/entry.mjs
+  }),
   vite: {
     plugins: [tailwindcss()]
   }
